9b98642a28ee7c165220ed7a5cc14bc6a8d63fa8
deeksha <deek.khadsare@gmail.com>
2018-02-23 01:44:07 -0600

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {Routes, RouterModule} from '@angular/router';
+import { NgxCarouselModule } from 'ngx-carousel';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
@@ -12,7 +13,15 @@ import { NewsComponent } from './menu/news/news.component';
 import { AcademicsComponent } from './menu/academics/academics.component';
 import { AdmissionsComponent } from './menu/admissions/admissions.component';
 import { CampuslifeComponent } from './menu/campuslife/campuslife.component';
+import { HomeComponent } from './menu/home/home.component';
 
+const appRoutes: Routes = [
+  {path : '', component : HomeComponent },
+  {path : 'news', component : NewsComponent } ,
+  {path : 'academics', component : AcademicsComponent } ,
+  {path : 'admissions', component : AdmissionsComponent } ,
+  {path : 'campuslife', component : CampuslifeComponent } ,
+  ];
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,12 +32,15 @@ import { CampuslifeComponent } from './menu/campuslife/campuslife.component';
     NewsComponent,
     AcademicsComponent,
     AdmissionsComponent,
-    CampuslifeComponent
+    CampuslifeComponent,
+    NgxCarouselModule,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/news/news.component.ts
+++ src/app/menu/news/news.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxCarouselModule } from 'ngx-carousel';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
+  public items: Object[] = [];
+  public carouselConfig:
+    NgxCarouselModule;
+  constructor() {
+    this.items = [
 
-  constructor() { }
+      {
 
+        title: '../../assets/images/slider/news1.jpg',
+
+        color: 'green'
+
+      },
+
+      {
+
+        title: '../../assets/images/slider/news4.jpg',
+
+        color: 'blue'
+
+      }
+
+    ]
+
+
+    this.carouselConfig = {
+
+      grid: {xs:
+          1, sm:
+          1, md:
+          3, lg:
+          1, all:
+          0},
+
+      slide: 1,
+
+      speed: 400,
+
+      interval:
+        10000,
+
+      point: {
+
+        visible:
+          true
+
+      },
+
+      loop: true,
+
+      touch: true
+
+    };
+
+  }
   ngOnInit() {
   }
-
 }
