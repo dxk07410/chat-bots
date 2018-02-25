b91cd7c4838c101225cb5ebe78b56b71865b33c9
deeksha <deek.khadsare@gmail.com>
2018-02-25 16:17:11 -0600

    comment

--- src/app/menu/academics/academics.component.ts
+++ src/app/menu/academics/academics.component.ts
@@ -1,14 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxCarouselModule } from 'ngx-carousel';
 @Component({
   selector: 'app-academics',
   templateUrl: './academics.component.html',
   styleUrls: ['./academics.component.css']
 })
 export class AcademicsComponent implements OnInit {
+  public items: Object[] = [];
+  public carouselConfig:
+    NgxCarouselModule;
+  constructor() {
+    this.items = [
 
-  constructor() { }
+      {
 
+        title: '../../../assets/images/academics/academics1.jpg',
+
+        color: 'green'
+
+      },
+
+      {
+
+        title: '../../../assets/images/academics/academics2.jpg',
+
+        color: 'blue'
+
+      },
+
+      {
+
+        title: '../../../assets/images/academics/academics3.jpg',
+
+        color: 'blue'
+
+      }
+
+    ]
+
+
+    this.carouselConfig = {
+
+      grid: {
+        xs:
+          1, sm:
+          1, md:
+          3, lg:
+          1, all:
+          0
+      },
+
+      slide: 1,
+
+      speed: 400,
+
+      interval:
+        6000,
+
+      point: {
+
+        visible:
+          true
+
+      },
+
+      loop: true,
+
+      touch: true
+
+    };
+
+  }
   ngOnInit() {
   }
 
