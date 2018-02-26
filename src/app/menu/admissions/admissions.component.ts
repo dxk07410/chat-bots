9216725a5aa95bb2618934def0e32d74ecc8a6c8
deeksha <deek.khadsare@gmail.com>
2018-02-26 00:31:40 -0600

    comment

--- src/app/menu/admissions/admissions.component.ts
+++ src/app/menu/admissions/admissions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgxCarouselModule } from 'ngx-carousel';
 @Component({
   selector: 'app-admissions',
   templateUrl: './admissions.component.html',
@@ -7,8 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmissionsComponent implements OnInit {
 
-  constructor() { }
+  public items: Object[] = [];
+  public carouselConfig:
+      NgxCarouselModule;
+    constructor()
+    {
+      this.items = [
 
+        {
+
+          title: '../../../assets/images/Admission/admission4.jpg',
+
+          color: 'green'
+
+        },
+
+        {
+
+          title: '../../../assets/images/Admission/admission3.jpg',
+
+          color: 'blue'
+
+        },
+
+        {
+
+          title: '../../../assets/images/Admission/admission1.jpg',
+
+          color: 'blue'
+
+        }
+
+      ]
+
+
+      this.carouselConfig = {
+
+        grid: {
+          xs:
+            1, sm:
+            1, md:
+            3, lg:
+            1, all:
+            0
+        },
+
+        slide: 1,
+
+        speed: 400,
+
+        interval:
+          6000,
+
+        point: {
+
+          visible:
+            true
+
+        },
+
+        loop: true,
+
+        touch: true
+
+      };
+
+    }
   ngOnInit() {
   }
 
